c8ea12cc59247c7ff568204577e2a0e979ba2851
Andrey Levkin <alevkin@gmail.com>
2019-10-10 14:56:26 +0300

    Linker changed to eosio.cdt lld

--- cicd/build.sh
+++ cicd/build.sh
@@ -8,6 +8,7 @@ NC='\033[0m'
 CORES=`getconf _NPROCESSORS_ONLN`
 mkdir -p build
 pushd build &> /dev/null
-cmake ../ -DCMAKE_MODULE_PATH=/opt/eosio/lib/cmake/haya -DCMAKE_LINKER=/usr/lib/llvm-4.0/bin/lld
-make -j${CORES}
+cmake -GNinja ../ -DCMAKE_MODULE_PATH=/opt/eosio/lib/cmake/haya -DCMAKE_LINKER=/usr/opt/eosio.cdt/1.6.1/bin/lld
+cmake --build .
+#make -j${CORES}
 popd &> /dev/null
