a17bfde5505d96d8ea32a42180ecdf3cff1afbf3
Andrew Petelin <adrianopol@gmail.com>
2020-05-13 11:59:02 +0300

    CYB-505 wrap voter_info::has_voted to binary extension
    Needed to unify mainnet and testnet chains (testnet was started without
    this field in voter_info structure). Field is unused since merging with
    eosio.contracts-1.8.x.

--- contracts/eosio.system/include/eosio.system/eosio.system.hpp
+++ contracts/eosio.system/include/eosio.system/eosio.system.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <eosio/asset.hpp>
+#include <eosio/binary_extension.hpp>
 #include <eosio/privileged.hpp>
 #include <eosio/singleton.hpp>
 #include <eosio/system.hpp>
@@ -256,7 +257,7 @@ namespace eosiosystem {
          cpu_managed = 4
       };
 
-      bool has_voted = false; // unused since merging with eosio.contracts-1.8.3
+      eosio::binary_extension<bool> has_voted; // unused since merging with eosio.contracts-1.8.3
 
       // explicit serialization macro is not necessary, used here only to improve compilation time
       EOSLIB_SERIALIZE( voter_info, (owner)(proxy)(producers)(staked)(last_vote_weight)(proxied_vote_weight)(is_proxy)(flags1)(reserved2)(reserved3)(has_voted) )
